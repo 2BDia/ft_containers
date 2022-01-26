3e406a15909f83425e46675962a94874d6dc5920
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2022-01-26 16:40:20 +0100

    firt push, resize can use reserve not coded yet

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/24 16:06:31 by rvan-aud          #+#    #+#             */
+/*   Updated: 2022/01/26 16:33:06 by rvan-aud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <algorithm>
+#include <vector>
+
+#include "vector.hpp"
+
+int	main()
+{
+	{		
+		std::cout << "*** FT ***" << std::endl;
+		ft::vector<int>	mine(10);
+		for (size_t i = 0; i < 10; i++)
+			mine.add(i, i);
+		mine.print();
+		std::cout << "size = " << mine.size() << std::endl;
+		std::cout << "max_size = " << mine.max_size() << std::endl;
+		mine.resize(5);
+		mine.resize(8, 100);
+		mine.resize(12);
+		mine.print();
+		std::cout << "size = " << mine.size() << std::endl;
+		std::cout << "capacity = " << mine.capacity() << std::endl;
+		mine.clear();
+		mine.print();
+		std::cout << "size = " << mine.size() << std::endl;
+
+	}
+	{
+		std::cout << std::endl << "*** STD ***" << std::endl;
+		std::vector<int> real(10);
+
+		for (size_t i = 0; i < real.size(); i++)
+			real[i] = i;
+		for (size_t i = 0; i < real.size(); i++)
+			std::cout << real[i] << std::endl;
+		std::cout << "size = " << real.size() << std::endl;
+		std::cout << "max_size = " << real.max_size() << std::endl;
+		real.resize(5);
+		real.resize(8, 100);
+		real.resize(12);
+		for (size_t i = 0; i < real.size(); i++)
+			std::cout << real[i] << std::endl;
+		std::cout << "size = " << real.size() << std::endl;
+		std::cout << "capacity = " << real.capacity() << std::endl;
+		real.clear();
+		std::cout << "size = " << real.size() << std::endl;
+	}
+	// system("leaks containers");
+}
