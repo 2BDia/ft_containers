c0bfff874cef86029b97dae68d307474547ee461
Julie Van Audenhaege <rvan-aud@fu-r8-p5.s19.be>
2022-02-08 13:07:17 +0100

    operators ok

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:06:31 by rvan-aud          #+#    #+#             */
-/*   Updated: 2022/02/08 12:03:36 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2022/02/08 13:03:33 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -554,11 +554,11 @@ int	main()
 
 		ft::vector<int>	one(5, 90);
 		ft::vector<int>	two(4, 100);
-		std::cout << (one < two) << std::endl;
+		std::cout << (one >= two) << std::endl;
 
 		std::vector<int>	three(5, 90);
 		std::vector<int>	four(4, 100);
-		std::cout << (three < four) << std::endl;
+		std::cout << (three >= four) << std::endl;
 	}
 	// system("leaks containers");
 }
