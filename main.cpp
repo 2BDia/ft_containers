151da60b849e42e458bdf552996bea80a2f923ff
Julie Van Audenhaege <rvan-aud@fu-r8-p5.s19.be>
2022-02-08 12:04:42 +0100

    did half of operations in vector

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:06:31 by rvan-aud          #+#    #+#             */
-/*   Updated: 2022/02/08 11:22:40 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2022/02/08 12:03:36 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -552,6 +552,13 @@ int	main()
 	{
 		std::cout << std::endl << "*** TEST ***" << std::endl;
 
+		ft::vector<int>	one(5, 90);
+		ft::vector<int>	two(4, 100);
+		std::cout << (one < two) << std::endl;
+
+		std::vector<int>	three(5, 90);
+		std::vector<int>	four(4, 100);
+		std::cout << (three < four) << std::endl;
 	}
 	// system("leaks containers");
 }
