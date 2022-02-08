cec681ec777bb2934b577eb897d7674fea2279d4
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2022-02-08 13:12:57 +0100

    vector should be done

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 15:17:05 by rvan-aud          #+#    #+#             */
-/*   Updated: 2022/02/08 13:03:12 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2022/02/08 13:10:37 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -390,4 +390,7 @@ namespace	ft
 
 	template <class T, class Alloc>
 	bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs) {return !(lhs < rhs);};
+
+	template <class T, class Alloc>
+	void swap (vector<T,Alloc>& x, vector<T,Alloc>& y) {x.swap(y);};
 }
