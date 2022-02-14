425f5ed42d46fdc671856ced67a1c0c488967ecb
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2022-02-14 09:52:01 +0100

    all vector tests ok, need to rework reverse iterators

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:06:31 by rvan-aud          #+#    #+#             */
-/*   Updated: 2022/02/10 15:09:50 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2022/02/14 09:46:09 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -552,7 +552,6 @@ int	main()
 	}
 	{
 		std::cout << std::endl << "*** TEST ***" << std::endl;
-
 		
 	}
 	// system("leaks containers");
