97e75482a7253244a61ea67950b56bcfb47d794e
Julie Van Audenhaege <rvan-aud@fu-r8-p5.s19.be>
2022-02-16 12:07:30 +0100

    started map

---
+++ map.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 11:59:39 by rvan-aud          #+#    #+#             */
+/*   Updated: 2022/02/16 12:07:06 by rvan-aud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+#include "iterator.hpp"
+#include "reverse_iterator.hpp"
+
+namespace	ft
+{
+	template < class Key, class T, class Compare = less<Key>,
+		class Alloc = allocator< pair<const Key,T>> >
+	class map
+	{
+		public:
+
+			typedef Key											key_type;
+			typedef T											mapped_type;
+			typedef ft::pair<key_type, mapped_type>				value_type;
+			typedef Compare										key_compare;
+			//value_compare to do
+			typedef Alloc										allocator_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+			//iterators and difference type
+			typedef size_t										size_type;
+	};
+}
